67bb0f822def914684f15a960e0cd62ffc7a0791
ksk <ksk@gmail.com>
2025-11-23 09:38:44 +0530

    update provider and var file with region

--- lambda-code/terraform/variables.tf
+++ lambda-code/terraform/variables.tf
@@ -5,3 +5,8 @@ variable "pg_user" { type = string }
 variable "pg_pass" { type = string }
 
 variable "pushgateway_url" { type = string }
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
